0624248077a488171bb20e61f9f3dbac97c28540
brfriedr <80418373+brfriedr@users.noreply.github.com>
2021-04-27 08:28:14 -0500

    Initial Upload

---
+++ dnac_config.py
@@ -0,0 +1,5 @@
+import os
+DNAC=os.environ.get('DNAC','10.91.43.70')
+DNAC_PORT=os.environ.get('DNAC_PORT',443)
+DNAC_USER=os.environ.get('DNAC_USER','brfriedr')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','gHipSShc7')
