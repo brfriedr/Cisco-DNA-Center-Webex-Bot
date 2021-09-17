c3e770d7d15bbfca737210ab889a230fe98e6ade
Brandon Friedrich <80418373+brfriedr@users.noreply.github.com>
2021-09-17 12:58:10 -0500

    Delete dnac_config.py

--- dnac_config.py
+++
@@ -1,5 +0,0 @@
-import os
-DNAC=os.environ.get('DNAC','10.91.43.70')
-DNAC_PORT=os.environ.get('DNAC_PORT',443)
-DNAC_USER=os.environ.get('DNAC_USER','brfriedr')
-DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','gHipSShc7')
